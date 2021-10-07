c15c542206d0b1f0bed03b795ea8b6e1e0e8785a
Nathanael Rosenheim <nrosenheim@arch.tamu.edu>
2021-10-07 09:12:15 -0500

    added class and help

--- _planning_methods.py
+++ _planning_methods.py
@@ -1,44 +1,46 @@
-"""
-Python File with Functions used in Planning Methods Course.
 
-version 0.0.1 2021-10-07
+class planningmethod():
+  """
+  Python File with Functions used in Planning Methods Course.
 
-"""
+  version 0.0.1 2021-10-07
 
-def obtain_census_api(
-                    state: str = "*",
-                    county: str = "*",
-                    census_geography: str = 'county:*',
-                    vintage: str = "2010", 
-                    dataset_name: str = 'dec/sf1',
-                    get_vars: str = 'GEO_ID'):
+  """
 
-        """General utility for obtaining census from Census API.
+  def obtain_census_api(
+                      state: str = "*",
+                      county: str = "*",
+                      census_geography: str = 'county:*',
+                      vintage: str = "2010", 
+                      dataset_name: str = 'dec/sf1',
+                      get_vars: str = 'GEO_ID'):
 
-        Args:
-            state (str): 2-digit FIPS code. Default * for all states
-            county (str): 3-digit FIPS code. Default * all counties
-            census_geography (str): example '&for=block:*' would be for all blocks
-              default is for all counties
-            vintage (str): Census Year. Default 2010
-            dataset_name (str): Census dataset name. Default Decennial SF1
-            for a list of all Census API
-            get_vars (str): list of variables to get from the API.
+          """General utility for obtaining census from Census API.
 
-        Returns:
-            obj, dict: A dataframe for with Census data
+          Args:
+              state (str): 2-digit FIPS code. Default * for all states
+              county (str): 3-digit FIPS code. Default * all counties
+              census_geography (str): example '&for=block:*' would be for all blocks
+                default is for all counties
+              vintage (str): Census Year. Default 2010
+              dataset_name (str): Census dataset name. Default Decennial SF1
+              for a list of all Census API
+              get_vars (str): list of variables to get from the API.
 
-        """
-        # Set up hyperlink for Census API
-        api_hyperlink = ('https://api.census.gov/data/' + vintage + '/'+dataset_name + '?get=' + get_vars +
-                        '&in=state:' + state + '&in=county:' + county + '&for=' + census_geography)
+          Returns:
+              obj, dict: A dataframe for with Census data
 
-        print("Census API data from: " + api_hyperlink)
+          """
+          # Set up hyperlink for Census API
+          api_hyperlink = ('https://api.census.gov/data/' + vintage + '/'+dataset_name + '?get=' + get_vars +
+                          '&in=state:' + state + '&in=county:' + county + '&for=' + census_geography)
 
-        # Obtain Census API JSON Data
-        apijson = requests.get(api_hyperlink)
+          print("Census API data from: " + api_hyperlink)
 
-        # Convert the requested json into pandas dataframe
-        df = pd.DataFrame(columns=apijson.json()[0], data=apijson.json()[1:])
+          # Obtain Census API JSON Data
+          apijson = requests.get(api_hyperlink)
 
-        return df
+          # Convert the requested json into pandas dataframe
+          df = pd.DataFrame(columns=apijson.json()[0], data=apijson.json()[1:])
+
+          return df
