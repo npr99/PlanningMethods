d6837e656c16a1bbbc81e81ecba400638813a88e
Nathanael Rosenheim <nrosenheim@arch.tamu.edu>
2021-10-18 17:04:57 -0500

    add clean variable function

--- _planning_methods.py
+++ _planning_methods.py
@@ -1,6 +1,7 @@
 
 import requests ## Required for the Census API
 import pandas as pd # For reading, writing and wrangling data
+import json # used to read in metadata for Census variables
 
 class planning_methods():
   """
@@ -47,3 +48,68 @@ class planning_methods():
           df = pd.DataFrame(columns=apijson.json()[0], data=apijson.json()[1:])
 
           return df
+
+
+  def clean_acs_variables(df,vintage,dataset_name,get_vars):
+    """
+    Function runs loop to rename variables, set variable type, and 
+    address missing values in ACS data.
+    """
+
+    for variable in get_vars.split(","):
+      variable_metadata_hyperlink = (f'https://api.census.gov/data/{vintage}/{dataset_name}/variables/{variable}.json')
+      # Obtain Census API JSON Data
+      !wget --no-cache --quiet --backups=1 {variable_metadata_hyperlink}
+
+      with open(f"{variable}.json", "r") as rf:
+        variable_metadata = json.load(rf)
+
+      # Find the variable label 
+      census_label_string = str(variable_metadata["label"])
+      last_exclamation_point_position = census_label_string.rfind("!!")
+      if last_exclamation_point_position >= 0:
+        last_exclamation_point_position = last_exclamation_point_position + 2
+      else:
+        last_exclamation_point_position = 0
+      label = census_label_string[last_exclamation_point_position:] 
+
+      # Add vintage to label name (skip geo_id and name variables)
+      if variable not in ['GEO_ID','NAME']:
+        label_addvintage = label + f' {vintage}'
+      else:
+        label_addvintage = label
+
+      # Add estimate or Margin of Error to label
+      last_letter_of_variable = variable[-1]
+      if variable not in ['GEO_ID','NAME']:
+        if last_letter_of_variable == 'E':
+          label_addvintage_addtype = label_addvintage + ' (Estimate)'
+        if last_letter_of_variable == 'M':
+          label_addvintage_addtype = label_addvintage + ' (MOE)'
+      else:
+        label_addvintage_addtype = label_addvintage
+      print(vintage,"Renameing",variable," = ",label_addvintage_addtype,"Changing type to",variable_metadata["predicateType"])
+
+      # Change variable type
+      df[variable] = df[variable].astype(variable_metadata["predicateType"])
+
+      # Reset Estimates and MOE with Annotation Values
+      Annotation_values = {-999999999 : 'Number of sample cases is too small.',
+      -888888888 : 'Estimate is not applicable or not available.',
+      -666666666 : 'No sample observations or too few sample observations were available to compute an estimate.',
+      -555555555 : 'Estimate is controlled. A statistical test for sampling variability is not appropriate.',
+      -333333333 : 'Median falls in the lowest interval or upper interval of an open-ended distribution. A statistical test is not appropriate.',
+      -222222222 : 'No sample observations or too few sample observations were available to compute a standard error and thus the margin of error. A statistical test is not appropriate.'}
+
+      for Annotation_value in Annotation_values:
+        observations_with_annotation = len(df.loc[(df[variable] == Annotation_value)])
+        if observations_with_annotation > 0:
+          print(observations_with_annotation,"Observations have",Annotation_value)
+          print(Annotation_values[Annotation_value])
+          print('Replacing values with missing.')
+          df.loc[(df[variable] == Annotation_value), variable] = np.nan
+
+
+      df = df.rename(columns={variable: label_addvintage_addtype}) 
+
+    return df
